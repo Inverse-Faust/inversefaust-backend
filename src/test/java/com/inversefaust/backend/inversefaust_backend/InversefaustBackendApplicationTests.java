6abac2c4c67cd7b785e0f3fd9876989421cd014b
KIM TAE HYEONG <thk12378905@gmail.com>
2024-09-05 15:53:41 +0900

    chore:초기 환경 추가

---
+++ src/test/java/com/inversefaust/backend/inversefaust_backend/InversefaustBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inversefaust.backend.inversefaust_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversefaustBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
